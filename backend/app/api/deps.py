a681226c1faac1c8d1d30f200c5801fa67eeefb7
Arnav S Rao <arnavsrao09@gmail.com>
2026-09-22 10:41:45 +0530

    fix dep issue

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -10,6 +10,7 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 import secrets
 import os
+import hashlib
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dummy-secret-key-for-hackathon-only")
 ALGORITHM = "HS256"
